efce6d706b7cd6fe948ed1cb4c31f0381b76a274
Steve Shi <shikangzhi@gmail.com>
2019-12-04 18:06:47 +0200

    Router use prefix API_V1 Add GET /auth/logout

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const sesseon = require('koa-session');
 const app = new Koa();
-const router = new Router();
 const PORT = 3000;
 const API_V1 = ''//'/api/v1'
+const router_v1 = new Router({ prefix: API_V1 });
 app.keys = ['\xa7%\xb2\x86Y\x02\xbdh\x02\x0fk\xcd\xdbQ\x1b\x0el\xb7g\x07\xe99Pl'];
 const passport = require('./auth');
 
@@ -16,8 +16,8 @@ function isAuthenticated(ctx, next) {
     //ctx.body = { status: 'error no login'};
 }
 
-const login_html =
-`<!DOCTYPE html>
+const login_html = `
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -33,25 +33,31 @@ const login_html =
 </body>
 </html>`
 
-router
-    .get(API_V1+'/auth/login', (ctx, next) => {
+router_v1
+    .get('/auth/login', (ctx, next) => {
         ctx.body = login_html;
     })
-    .get(API_V1+'/auth/status', isAuthenticated, (ctx, next) => {
-        if (ctx.isAuthenticated())
-            ctx.body = 'success login';
-        else
-            ctx.body = 'no login';
+    .get('/auth/status', isAuthenticated, (ctx, next) => {
+        ctx.body = ctx.state.user;
     })
-    .post(API_V1+'/auth/login', (ctx, next) => {
+    .post('/auth/login', (ctx, next) => {
         return passport.authenticate('local', (err, user, info, status) => {
             if (user) {
                 ctx.login(user);
                 ctx.redirect(API_V1+'/auth/status');
+                console.log('LOGIN: Success: ' + user.username);
             } else {
                 ctx.status = 400;
             }
         })(ctx);
+    })
+    .get('/auth/logout', (ctx, next) => {
+        if (ctx.isAuthenticated()) {
+            console.log('LOGOUT: ' + ctx.state.user.username);
+            ctx.logout();
+        }
+        ctx.body = { status: 'success' };
+        ctx.status = 200;
     })
 
 app
@@ -59,8 +65,8 @@ app
     .use(sesseon(app))
     .use(passport.initialize())
     .use(passport.session())
-    .use(router.routes())
-    .use(router.allowedMethods())
+    .use(router_v1.routes())
+    .use(router_v1.allowedMethods())
     .listen(PORT, () => {
         console.log('Server is listening on port: ' + PORT);
     });
